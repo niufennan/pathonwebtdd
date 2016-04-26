bba47ab80cfd9eb200c5e93f4c78bd1c8d906025
niufennan <jiangchao226@126.com>
2016-04-27 00:21:20 +0800

    p88

--- lists/tests.py
+++ lists/tests.py
@@ -11,11 +11,6 @@ from lists.models import Item
 class HomePageTest(TestCase):
 	"""docstring for SmokeTest"""
 
-	def test_home_page_only_saves_items_when_necessary(self):
-		request=HttpRequest()
-		home_page(request)
-		self.assertEqual(Item.objects.count(),0)
-
 	def test_root_url_resolves_to_home_page_view(self):
 		found=resolve("/")
 		self.assertEqual(found.func,home_page)	
@@ -31,18 +26,47 @@ class HomePageTest(TestCase):
 		#self.assertIn(b"<title>To-Do lists</title>",response.content)
 		#self.assertTrue(response.content.strip().endswith(b"</html>"))
 
-	def test_home_page_can_save_a_POST_request(self):
-		request=HttpRequest()
-		request.method="POST"
-		request.POST["item_text"]="A new list item"
+	
 
-		response=home_page(request)
+	
 
 
+class NewListTest(TestCase):
 
+	def test_redirects_after_POST(self):
+		response=self.client.post(
+			"/lists/new",
+			data={"item_text":"A new list item"}
+			)
+
+		self.assertEqual(response.status_code,302)
+		self.assertEqual(response["location"],"/lists/the-only-list-in-the-world/")
+
+		#request=HttpRequest()
+		#request.method="POST"
+		#request.POST["item_text"]="A new list item"
+
+		#response=home_page(request)
+
+		#self.assertEqual(response.status_code,302)
+		#self.assertEqual(response["location"],"/lists/the-only-list-in-the-world/")
+
+	def test_saving_a_POST_request(self):
+		self.client.post(
+			"/lists/new",
+			data={"item_text":"A new list item"}
+			)
+		#request=HttpRequest()
+		#request.method="POST"
+		#request.POST["item_text"]="A new list item"
 		self.assertEqual(Item.objects.count(),1)
 		new_item=Item.objects.first()
 		self.assertEqual(new_item.text,"A new list item")
+
+		#response=home_page(request)
+		#self.assertEqual(Item.objects.count(),1)
+		#new_item=Item.objects.first()
+		#self.assertEqual(new_item.text,"A new list item")
 
 		
 		#self.assertIn("A new list item",response.content.decode())
@@ -53,17 +77,7 @@ class HomePageTest(TestCase):
 		#print(expected_html)
 		#self.assertEqual(response.content.decode(),expected_html)
 
-	def test_home_page_redirects_after_POST(self):
-		request=HttpRequest()
-		request.method="POST"
-		request.POST["item_text"]="A new list item"
-
-		response=home_page(request)
-
-		self.assertEqual(response.status_code,302)
-		self.assertEqual(response["location"],"/lists/the-only-list-in-the-world/")
-
-
+		
 	
 class ListViewTest(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -5,19 +5,13 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-	#if request.method=="POST":
-	#	return HttpResponse(request.POST["item_text"])
-	
-	if request.method=="POST":
-		#new_item_text=request.POST["item_text"]
-		Item.objects.create(text=request.POST["item_text"])
-		return redirect("/lists/the-only-list-in-the-world/")
-	#else :
-		#new_item_text=""
-	
 	return render(request,"home.html")
 
 def view_list(request):
 	items=Item.objects.all()
 
 	return render(request,"list.html",{'items':items})
+
+def new_list(request):
+	Item.objects.create(text=request.POST["item_text"])
+	return redirect("/lists/the-only-list-in-the-world/")
