58dac0eec7bfad56a092819ca8ab7fdde8f95128
niufennan <jiangchao226@126.com>
2016-05-05 23:17:37 +0800

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,29 @@
 # -*-coding:utf8-*-
 
+import sys
 from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
-	"""docstring for NewVisitorTest"""
+	
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if "liveserver" in arg:
+				cls.server_url="http://"+arg.split("=")[1]
+				return
+		super().setUpClass()
+		cls.server_url=cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url==cls.live_server_url:
+			super().tearDownClass()
+
+
+
 	def setUp(self):
 		self.browser=webdriver.Firefox()
 		self.browser.implicitly_wait(13)
@@ -20,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#伊迪斯访问首页
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,786)
 		#他看到输入框完美的剧中显示
 		inputbox=self.browser.find_element_by_id("id_new_item")
@@ -32,7 +49,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrivev_it_later(self):
 		#伊利斯听说有一个很酷的在线代办事项应用
 		#她去看了看这个应用的首页
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		#她注意到网页的标题和头部都包含”To-Do“这个词
 		self.assertIn( "To-Do", self.browser.title)# , "Browser title was" +browser.title
@@ -84,7 +101,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 		#弗朗西斯访问首页
 		#页面中看不到伊迪斯的清单
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text=self.browser.find_element_by_tag_name("body").text
 		self.assertNotIn("Buy peacock feathers", page_text)
 		self.assertNotIn("make a fly",page_text)
